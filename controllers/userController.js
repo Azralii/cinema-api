a2f99949d4d8173e1ec1f3f1b54c0427a0a0200e
Azralii <111491491+Azralii@users.noreply.github.com>
2023-11-09 09:53:21 +0100

    Jag korrigerad cinema.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import bcrypt from 'bcryptjs'
 
 
 export const signUp = async(req,res)=>{
@@ -6,7 +7,8 @@ const {name,email,password}=req.body;
 if(name===""||email==="" || password===''){
     res.json({message:'every field must be filled'})
 }
-await User.create({name,email,password})
+const hashPassword = await bcrypt.hash(password,10)
+await User.create({name,email,password:hashPassword})
 res.json({message:'registration successful'});
 }
 
